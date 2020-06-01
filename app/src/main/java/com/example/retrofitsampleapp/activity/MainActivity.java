02a33a802ac86e4a1a155dc709c6c0728547d4de
nehabalaji <nehabalaji10@gmail.com>
2020-06-01 20:17:45 +0530

    Consume the REST web service

--- app/src/main/java/com/example/retrofitsampleapp/activity/MainActivity.java
+++ app/src/main/java/com/example/retrofitsampleapp/activity/MainActivity.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.retrofitsampleapp.R;
+import com.example.retrofitsampleapp.model.Movie;
 import com.example.retrofitsampleapp.network.ApiClient;
 import com.example.retrofitsampleapp.network.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<List<Movie>> call = apiService.getMovies();
+        call.enqueue(new Callback<List<Movie>>() {
+            @Override
+            public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
+                
+            }
+
+            @Override
+            public void onFailure(Call<List<Movie>> call, Throwable t) {
+
+            }
+        });
     }
 }
